3ca5bcf2ef63fbbd6ef2dfc9536acf2402ee7b27
yanxinc <yanxinc@andrew.cmu.edu>
2023-12-11 17:52:12 -0500

    fix client

--- src/client/client.py
+++ src/client/client.py
@@ -11,12 +11,22 @@ class RedditClient:
         self.channel = grpc.insecure_channel(f'{host}:{port}')
         self.stub = reddit_pb2_grpc.RedditServiceStub(self.channel)
 
-    def create_post(self, post_req):
-        response = self.stub.CreatePost(post_req)
+    def create_post(self, post_data):
+        post = reddit_pb2.CreatePostRequest(
+            title= post_data["title"],
+            text= post_data["text"],
+            image_url= "/path/to/image.img"
+        )
+        if "author" in post_data: post.author = reddit_pb2.User(userID=post_data["author"])
+        if "video_url" in post_data: post.video_url = post_data["video_url"]
+        elif "image_url" in post_data: post.image_url = post_data["image_url"]
+
+        response = self.stub.CreatePost(post)
         return response
     
-    def vote_post(self, post_id, vote_type):
-        request = reddit_pb2.VotePostRequest(post_id=post_id, vote_type=vote_type)
+    def vote_post(self, post_id, isUpvote):
+        vote = reddit_pb2.UPVOTE if isUpvote else reddit_pb2.DOWNVOTE
+        request = reddit_pb2.VotePostRequest(post_id=post_id, vote_type=vote)
         response = self.stub.VotePost(request)
         return response
     
@@ -25,12 +35,21 @@ class RedditClient:
         response = self.stub.GetPost(request)
         return response
     
-    def create_comment(self, comment_req):
-        response = self.stub.CreateComment(comment_req)
+    def create_comment(self, comment_data):
+        comment = reddit_pb2.CreateCommentRequest(
+            author=reddit_pb2.User(userID=comment_data["author"]),
+            text=comment_data["text"]
+        )
+
+        if "comment_id" in comment_data: comment.comment_id = comment_data["comment_id"]
+        elif "post_id" in comment_data: comment.post_id = comment_data["post_id"]
+
+        response = self.stub.CreateComment(comment)
         return response
     
-    def vote_comment(self, comment_id, vote_type):
-        request = reddit_pb2.VoteCommentRequest(comment_id=comment_id, vote_type=vote_type)
+    def vote_comment(self, comment_id, isUpvote):
+        vote = reddit_pb2.UPVOTE if isUpvote else reddit_pb2.DOWNVOTE
+        request = reddit_pb2.VoteCommentRequest(comment_id=comment_id, vote_type=vote)
         response = self.stub.VoteComment(request)
         return response
     
@@ -53,16 +72,17 @@ class RedditClient:
 
 #     # Create a Post
 #     print("============ Create a post ============")
-#     post = reddit_pb2.CreatePostRequest(
-#         title= "Post1",
-#         # author= reddit_pb2.User(userID="cindy2"),
-#         text= "My first Post",
-#         image_url= "/path/to/image.img")
-#     client.create_post(post)
+#     post_data = {
+#         "title": "Post123",
+#         "text": "post post post",
+#         "image_url": "/path/to/image.img",
+#         # "author": 'cindy2'
+#     }
+#     print(client.create_post(post_data))
 
 #     # Upvote or downvote a Post
 #     print("============ Upvote or downvote a Post ============")
-#     client.vote_post(0, reddit_pb2.DOWNVOTE)
+#     print(client.vote_post(0, False))
 
 #     # Retrieve Post content
 #     print("============ Retrieve Post content ============")
@@ -70,18 +90,18 @@ class RedditClient:
 #     print(post)
 
 #     # Create a Comment
-#     print("============ Create a Comment ============")
-#     comment = reddit_pb2.CreateCommentRequest(
-#         post_id=0,
-#         author=reddit_pb2.User(userID="cindy"),
-#         text="hello"
-#     )
-#     comment_rsp = client.create_comment(comment)
-#     print(comment_rsp)
+    # print("============ Create a Comment ============")
+    # comment_data = {
+    #     "post_id":0,
+    #     "author":"cindy",
+    #     "text":"hello"
+    # }
+    # comment_rsp = client.create_comment(comment_data)
+    # print(comment_rsp)
 
 #     # Upvote or downvote a Comment
 #     print("============ Upvote or downvote a Comment ============")
-#     client.vote_comment(0, reddit_pb2.UPVOTE)
+#     print(client.vote_comment(0, True))
 
 #     # Retrieving a list of N most upvoted comments under a post
 #     print("============ Retrieving a list of N most upvoted comments under a post ============")
